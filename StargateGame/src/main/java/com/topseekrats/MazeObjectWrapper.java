c68c7f4acd9384fedb9ada9b40189fcf6576964d
topseekrats <loveszglab4@gmail.com>
2016-04-24 23:11:21 +0200

    Map_1

--- StargateGame/src/main/java/com/topseekrats/MazeObjectWrapper.java
+++ StargateGame/src/main/java/com/topseekrats/MazeObjectWrapper.java
@@ -12,6 +12,15 @@ public class MazeObjectWrapper {
     private Stack<Foreground> foregrounds = new Stack<Foreground>();
     private Replicator replicator = null;
 
+    public MazeObjectWrapper() {}
+
+    public MazeObjectWrapper( Background background) { this.background = background; }
+
+    public MazeObjectWrapper( Background background, Foreground foreground) {
+        this.background = background;
+        foregrounds.push(foreground);
+    }
+
     public void setActor(int id, Actor actor) { actors[id] = actor; }
     public Actor getActor(ActorType type) { return actors[type.ordinal()]; }
 
