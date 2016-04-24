a51cb11e9afb284e85582f8ca4704048b493a8f0
topseekrats <loveszglab4@gmail.com>
2016-04-24 14:36:45 +0200

    Maze már egy singleton-féleség, hogy lehessen szerializálni, Engine save, load (szerializálás) kész

--- StargateGame/src/main/java/com/topseekrats/Engine.java
+++ StargateGame/src/main/java/com/topseekrats/Engine.java
@@ -1,18 +1,33 @@
 package com.topseekrats;
 
-public class Engine {
-    public void newGame() {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public final class Engine {
+
+    public static void newGame() {
     }
 
-    public void save() {
+    public static void save() throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("maps/default.sgmap"));
+        out.writeObject(Maze.getInstance());
+        out.close();
     }
 
-    public void load() {
+    public static Maze load() throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("maps/default.sgmap"));
+        Maze temp = (Maze)in.readObject();
+        in.close();
+        return temp;
     }
 
-    public void victory() {
+    public static void victory(ActorType actorType) {
     }
 
-    public void death() {
+    public static void death(ActorType actorType) {
     }
+
 }
