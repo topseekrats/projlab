de4a9a5770d27f7f5c56a1b97ffc79b3c2af9fc5
topseekrats <loveszglab4@gmail.com>
2016-04-28 08:46:36 +0200

    MazePanel matTest.txt-ből olvas (default.sgmap -ből nem sikerült előállítani Maze-t) Log.java: Log.log("s") = console println() segéd

--- StargateGame/src/main/java/com/topseekrats/Tester.java
+++ StargateGame/src/main/java/com/topseekrats/Tester.java
@@ -107,9 +107,7 @@ public class Tester {
                 break;
             case 0:
                 // GUI
-
                 Frame jf = new Frame();
-
                 break;
             default:
                 System.out.println("Invalid test case.");
